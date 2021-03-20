b9e2e00c3a12e5d32ef69b0044db2c0f734cb41e
JustABanana <JustABanana@users.noreply.github.com>
2021-03-20 23:03:38 +0100

    ✨ Display the stations on the map

--- source/app.d
+++ source/app.d
@@ -3,20 +3,17 @@ module konsola_operatorska.app;
 
 import gtk.MainWindow;
 import gtk.Main;
-import gtk.ListStore;
-import gtk.TreeIter;
 import gtk.Application;
 import gtk.Box;
+import gtk.IconTheme;
 
 import konsola_operatorska.assets;
 import konsola_operatorska.header;
 import konsola_operatorska.basestation;
 import konsola_operatorska.basestation_model;
 import konsola_operatorska.basestation_treeview;
-
 import konsola_operatorska.error_triangle;
-
-
+import konsola_operatorska.mapview;
 
 class AdminConWindow : MainWindow
 {
@@ -31,9 +28,12 @@ class AdminConWindow : MainWindow
         Box box = new Box(GtkOrientation.HORIZONTAL, 10);
 
         auto model = new BaseStationModel("http://localhost:8080/radios");
+
         auto bs_tv = new StationTreeView(model);
+        auto smv = new StationMap(model);
 
         box.add(bs_tv);
+        box.add(smv);
         add(box);
 
         auto header = new Header(model);
